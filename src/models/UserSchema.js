0356580d92eab7ed1d780da659432cb5e593392e
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-01-27 15:05:23 -0600

    Add: User Schema

---
+++ src/models/UserSchema.js
@@ -0,0 +1,24 @@
+const { model, Schema } = require('mongoose')
+
+const userSchema = new Schema({
+	user_photo: String,
+	image_bg: String,
+	name: String,
+	username: String,
+	description: String,
+	joined_date: { type: Date, default: new Date() },
+	following: Array,
+	followers: Array
+})
+
+userSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
