01f00b11c8eaa1725525fab86814f6d41a8089c8
Mario Quiros Luna <marioquiros21@yahoo.com>
2022-02-08 12:49:51 -0600

    Add: Reply comment on post

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -1,5 +1,6 @@
 const postRouter = require('express').Router()
 const Post = require('../models/PostSchema')
+const User = require('../models/UserSchema')
 
 /**
  * GET
@@ -49,6 +50,57 @@ postRouter.post('/', async (req, res, next) => {
 	} catch (error) {
 		next(error)
 	}
+})
+
+postRouter.put('/comment/:username/:id', async (req, res, next) => {
+	const {
+		username,
+		id,
+	} = req.params
+	const {
+		userComment,
+		comment,
+		media
+	} = req.body
+
+	try {
+		let post = await Post.findOne({ _id: id, username: username })
+		let userNewComment = await User.findOne({ username: userComment })
+
+		if (!post) return res.status(404).send({ error: 'post not found' })
+
+		let newComment = {
+			user_photo: userNewComment.user_photo,
+			nameUser: userNewComment.nameUser,
+			username: userNewComment.username,
+			postTime: new Date(),
+			text_posted: comment,
+			media_posted: media,
+			comments: [],
+			retweets: [],
+			likes: []
+		}
+
+		let newPostInfo = {
+			user_photo: post.user_photo,
+			nameUser: post.nameUser,
+			username: post.username,
+			postTime: post.postTime,
+			text_posted: post.text_posted,
+			media_posted: post.media_posted,
+			comments: [].concat(newComment).concat(post.comments),
+			retweets: [],
+			likes: []
+		}
+
+		post = await Post.findByIdAndUpdate(id, newPostInfo, { new: true })
+
+		res.json(post)
+
+	} catch (error) {
+		next(error)
+	}
+
 })
 
 module.exports = postRouter
